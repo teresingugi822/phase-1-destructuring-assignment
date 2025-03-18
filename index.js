a977f20fa778d092d5c961a0493b261e25ffed5c
TeresiNN <teresianyathira2007@gmail.com>
2025-03-18 12:29:15 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const [r, o, y, g, b, , v] = colors;
+
+const [, , , , , indg] = colors;
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +22,9 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const { muppetName, color, song, job, partner } = muppet;
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +40,14 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {
+  album: {
+    theMuppetMovie: { song2, song4 },
+  },
+  nestedJob,
+  nestedPartner,
+} = nestedMuppet;
 
 // Strings
 
